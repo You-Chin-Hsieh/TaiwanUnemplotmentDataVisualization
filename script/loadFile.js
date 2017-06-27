58cbb775d5eba456e8b045d63fbd0098a8a7827c
peter <p50305peter@gmai.com>
2017-06-27 09:18:18 +0800

    final
    final

--- script/loadFile.js
+++ script/loadFile.js
@@ -72,7 +72,28 @@ function refresh(countys,attributes,range){
             var color = d3.scale.linear().domain([min,max]).range(range);
             //console.log(color)
 
-        fillColor(color,attributes)
+        fillColor(color,attributes);
+            var width  = "100%",
+                height = 10,
+                padding = 80;
+
+            var w = 140, h = 400;
+
+            var key = d3.select("body").append("svg").attr("width", w).attr("height", h);
+
+            var legend = key.append("defs").append("svg:linearGradient").attr("id", "gradient").attr("x1", "100%").attr("y1", "0%").attr("x2", "100%").attr("y2", "100%").attr("spreadMethod", "pad");
+
+            legend.append("stop").attr("offset", "0%").attr("stop-color", color(max)).attr("stop-opacity", 1);
+
+            legend.append("stop").attr("offset", "100%").attr("stop-color", color(min)).attr("stop-opacity", 1);
+
+            key.append("rect").attr("width", w - 100).attr("height", h - 100).style("fill", "url(#gradient)").attr("transform", "translate(0,10)");
+
+            var y = d3.scale.linear().range([300, 0]).domain([min, max]);
+
+            var yAxis = d3.svg.axis().scale(y).orient("right");
+
+            key.append("g").attr("class", "y axis").attr("transform", "translate(41,10)").call(yAxis).append("text").attr("transform", "rotate(-90)").attr("y", 30).attr("dy", ".71em").style("text-anchor", "end");
 
     });
 })}
